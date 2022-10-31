b29455eeed2735907a26b990323178f142ed1e26
Brais Fernandez <braisoncrece@gmail.com>
2022-10-31 17:27:12 +0100

    Starting with translations I18n, and installing devise

--- app/models/task.rb
+++ app/models/task.rb
@@ -12,4 +12,14 @@
 #
 class Task < ApplicationRecord
   belongs_to :category
+
+  validates :name, :description, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
+  validate :due_date_validity
+
+  def due_date_validity
+    if due_date.present? && due_date < Date.today
+      errors.add(:due_date, I18n.t("task.errors.due_date"))
+    end
+  end
 end
