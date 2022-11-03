6ef866bf91b5ff7c3b3442108aa77961635f11be
Brais Fernandez <braisoncrece@gmail.com>
2022-11-03 11:04:40 +0100

    Styles v2

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,14 @@
 module TasksHelper
+  def task_badge(task)
+    case task.category.name 
+    when "desarrollo"
+      "badge text-bg-primary"
+    when "mercadeo" 
+      "badge text-bg-success"
+    when "conceptualización"
+      "badge text-bg-warning"
+    when "ejercicios"
+      "badge text-bg-danger"
+    end
+  end
 end
